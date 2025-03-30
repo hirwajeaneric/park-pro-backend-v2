e8b14e49bcd2c1976f656c993dc4d94979c79826
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-03-30 15:10:24 +0200

    setup: Project Initialization

---
+++ src/main/java/com/park/parkpro/ParkProApplication.java
@@ -0,0 +1,13 @@
+package com.park.parkpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/park/parkpro/ParkProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.parkpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
