3795b83a799339ce7d77c93c9e952e7151854517
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-03-31 09:52:53 +0200

    feature: Park Controller (create method)

--- src/main/java/com/park/parkpro/ParkController.java
+++ src/main/java/com/park/parkpro/ParkController.java
@@ -1,9 +1,30 @@
 package com.park.parkpro;
 
+import com.park.parkpro.domain.Park;
+import com.park.parkpro.service.ParkService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/parks")
 public class ParkController {
+    private final ParkService parkService;
+
+    public ParkController(ParkService parkService) {
+        this.parkService = parkService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Park> createPark(@RequestBody Park park) {
+        Park createdPark = parkService.createPark(park);
+        // Return 201 with Location header
+        return ResponseEntity
+                .created(URI.create("/api/parks/" + createdPark.getId()))
+                .body(createdPark);
+    }
 }
